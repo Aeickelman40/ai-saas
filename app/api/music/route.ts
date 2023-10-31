b52447b4fb9d254d42f1b743426421a940d4b7ee
Alex Eickelman <57731927+Aeickelman40@users.noreply.github.com>
2023-10-30 19:34:17 -0700

    fix music route error

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -30,10 +30,6 @@ export async function POST(req: Request) {
       return new NextResponse("Free trial has expired", { status: 403 });
     }
 
-    if (!freeTial) {
-      return new NextResponse("Free trial has expired", { status: 403 });
-    }
-
     const response = await replicate.run(
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
